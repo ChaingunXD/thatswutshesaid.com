d9c2c297dfe78fb34ecf78d5aeb7e470768e06e0
Troy Whiteley <troy@somanyscientists.com>
2013-06-30 17:31:42 -0700

    Added background images.

--- templates/joke.php
+++ templates/joke.php
@@ -1,7 +1,9 @@
-<div id="joke">
-  <h1><?php echo htmlspecialchars($joke['title']); ?>
+<h1 class="joke"><?php echo htmlspecialchars($joke['title']); ?></h1>
+
+<div class="tagline">
+  That's what <?php echo ($current_gender === 'f') ? 'she' : 'he'; ?> said!
 </div>
 
-<div id="more">
+<div class="more">
   <a href="/">Again!</a>
 </div>
--- templates/layout.php
+++ templates/layout.php
@@ -11,6 +11,19 @@
       echo $og_content;
     }
     ?>
+
+    <link rel="stylesheet" href="/ui/css/styles.css">
+    <style type="text/css">
+      body {
+        <?php if($current_gender === 'f'): ?>
+        background-image: url(/ui/images/background-hires-f.jpg);
+        background-color: #864486;
+        <?php else: ?>
+        background-image: url(/ui/images/background-hires-m.jpg);
+        background-color: #3e4f7a;
+        <?php endif; ?>
+      }
+    </style>
   </head>
   <body>
     <?php echo $content; ?>
